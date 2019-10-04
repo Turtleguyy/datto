f4677eaa36f6788338766603018e06f11b1e37c4
Zachary Nicoll <github@zdn.me>
2019-10-04 17:46:54 -0500

    Sass and pods

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'datto',
+    podModulePrefix: 'datto/pods',
     environment,
     rootURL: '/',
     locationType: 'auto',
