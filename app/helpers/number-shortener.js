1d59d019c4a25c31fb7dfdde4c5f0f0b6ce5c95c
Zachary Nicoll <github@zdn.me>
2019-10-11 16:58:25 -0500

    Add twitch followers

--- app/helpers/number-shortener.js
+++ app/helpers/number-shortener.js
@@ -5,12 +5,17 @@ export function numberShortener(number) {
   if (isNaN(number)) return
 
   if (number > 1000000) {
-    return `${number / 1000000}M`
+    return `${round((number / 1000000), 1)}M`
   } else if (number > 1000) {
-    return `${number / 1000}K`
+    return `${Math.round(number / 1000)}K`
   } else {
     return `${number}`
   }
+}
+
+function round(value, precision) {
+  let multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
 }
 
 export default helper(numberShortener)
--- app/pods/application/route.js
+++ app/pods/application/route.js
@@ -4,19 +4,40 @@ import fetch from 'fetch'
 
 export default Route.extend({
   model() {
+
+    // twitch
+    const client = 'zvp7v759yk8qscey6q06xh6mr4ev69'
+    const twitchURL = 'https://api.twitch.tv/helix/users?login=datto'
+
+    // twitter
+    const twitterURL = 'https://api.twitter.com/1.1/users/show.json?screen_name=DattosDestiny'
+
+    // youtube
     const part = 'statistics'
     const id   = 'UCkmAjbIecTIjTQctolYeuDw'
     const key  = 'AIzaSyBgOn1bSc2CcLUywtttJ4OWSrejZaGsKCM'
+    const youtubeURL = `https://www.googleapis.com/youtube/v3/channels?part=${part}&id=${id}&key=${key}`
 
     return RSVP.hash({
-      twitch: '',
-      twitter: '',
-      youtube: fetch(`https://www.googleapis.com/youtube/v3/channels?part=${part}&id=${id}&key=${key}`)
+      twitch: fetch(twitchURL, {
+        headers: {
+          'Client-ID': 'zvp7v759yk8qscey6q06xh6mr4ev69',
+        }
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        if (data.data.length) return data.data[0].view_count
+      }),
+      // twitter: fetch(twitterURL, {
+      //   headers: {
+      //     'authorization': `Bearer ${id}`,
+      //   }
+      // }),
+      youtube: fetch(youtubeURL)
         .then(response => response.json())
         .then(data => {
-          if (data.items.length) {
-            return data.items[0].statistics.subscriberCount
-          }
+          if (data.items.length) return data.items[0].statistics.subscriberCount
         })
     })
   }
