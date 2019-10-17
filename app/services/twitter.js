18dc02ec876a08ced2c50b3b79e5eb22fe332a86
Zachary Nicoll <github@zdn.me>
2019-10-17 16:58:28 -0500

    Mmmmmm, dun 4 now

--- app/services/twitter.js
+++ app/services/twitter.js
@@ -7,6 +7,7 @@ export default Service.extend({
 
     this.api = new Twit({
       consumer_key:    'd57GN3Eul9miMhh5WZHrs6mMZ',
+      consumer_secret: 'jWcQ56wpL2W0s1WwS8R0vcNQ2SZDv8G4nGrwFiCLwCyTMOLIwi',
       app_only_auth:   true,
     })
   },
